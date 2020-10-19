2c1db1d6f8858ea609715f43d6b591f2275d169d
Yeji Han <hany7@hawaii.edu>
2020-10-19 03:12:54 -1000

    Section 6

--- app/imports/ui/App.jsx
+++ app/imports/ui/App.jsx
@@ -1,35 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
-import { Task } from './Task';
 import { TasksCollection } from '/imports/api/TasksCollection';
+import { Task } from './Task';
 import { TaskForm } from './TaskForm';
 
 const toggleChecked = ({ _id, isChecked }) => {
   TasksCollection.update(_id, {
     $set: {
-      isChecked: !isChecked
-    }
-  })
+      isChecked: !isChecked,
+    },
+  });
 };
 
 const deleteTask = ({ _id }) => TasksCollection.remove(_id);
 
-
 export const App = () => {
-  const tasks = useTracker(() => TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch())
+  const [hideCompleted, setHideCompleted] = useState(false);
+
+  const hideCompletedFilter = { isChecked: { $ne: true } };
+
+  const tasks = useTracker(() =>
+      TasksCollection.find(hideCompleted ? hideCompletedFilter : {}, {
+        sort: { createdAt: -1 },
+      }).fetch()
+  );
+
+  const pendingTasksCount = useTracker(() =>
+      TasksCollection.find(hideCompletedFilter).count()
+  );
+
+  const pendingTasksTitle = `${
+      pendingTasksCount ? ` (${pendingTasksCount})` : ''
+  }`;
 
   return (
       <div className="app">
         <header>
           <div className="app-bar">
             <div className="app-header">
-              <h1>Welcome to Meteor!</h1>
+              <h1>
+                📝️ To Do List
+                {pendingTasksTitle}
+              </h1>
             </div>
           </div>
         </header>
 
         <div className="main">
           <TaskForm />
+
+          <div className="filter">
+            <button onClick={() => setHideCompleted(!hideCompleted)}>
+              {hideCompleted ? 'Show All' : 'Hide Completed'}
+            </button>
+          </div>
 
           <ul className="tasks">
             {tasks.map(task => (
